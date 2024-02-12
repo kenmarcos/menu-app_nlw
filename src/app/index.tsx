6abaaa2ec6787619573d16ded5ded28d9cbf9922
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-02-12 18:30:08 -0300

    feat: add initial Header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from "@/components/header";
 import { Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View className="flex-1 pt-10">
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" />
       <Text className="text-red-500 text-4xl">Home Page</Text>
     </View>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+import { Feather } from "@expo/vector-icons";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import colors from "tailwindcss/colors";
+
+interface HeaderProps {
+  title: string;
+  cartQuantity?: number;
+}
+
+export const Header = ({ title, cartQuantity = 0 }: HeaderProps) => {
+  return (
+    <View className="px-5 py-5 border-b border-b-slate-700 flex-row items-center justify-between">
+      <View className="">
+        <Image source={require("@/assets/logo.png")} className="h-6 w-32" />
+
+        <Text className="text-white text-xl font-heading">{title}</Text>
+      </View>
+
+      {cartQuantity > 0 && (
+        <TouchableOpacity className="relative" activeOpacity={0.7}>
+          <Feather name="shopping-bag" size={24} color={colors.white} />
+
+          <View className="bg-lime-300 w-5 h-5 rounded-full items-center justify-center absolute -top-2.5 -right-2.5">
+            <Text className="text-xs text-slate-900 font-bold">
+              {cartQuantity}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
