a015047ef97c4c28a0f785f30a1789354be27aed
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-02-12 17:28:11 -0300

    chore: add initial setup

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View className="bg-slate-900 flex-1">
+      <Text className="text-red-500 text-4xl">Home</Text>
+    </View>
+  );
+};
+
+export default Home;
