dba9f2b79d015d68f7f88415003be8776ac841c3
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-02-12 17:59:54 -0300

    feat: add Layout and Loading components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-red-500 text-4xl">Home</Text>
+    <View className="flex-1 pt-10">
+      <Text className="text-red-500 text-4xl">Home Page</Text>
     </View>
   );
 };
